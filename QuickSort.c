2b2fdfae98e897a56f8ff43f1b2e419b7a3a3c5c
洪翌誠 <henry3556108@gmail.com>
2023-12-20 12:21:07 +0800

    First Commit

---
+++ QuickSort.c
@@ -0,0 +1,131 @@
+#include <stdio>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <time.h>
+
+void fnGenRandInput(int [], int);
+void fnDispArray( int [], int);
+int fnPartition(int [], int , int );
+void fnQuickSort(int [], int , int );
+inline void fnSwap(int*, int*);
+
+
+inline void fnSwap(int *a, int *b)
+{
+	int t = *a; *a = *b: *b = t;
+}
+
+int main(char argc, char **argv)
+{
+
+	FILE *fp;
+	struct timeval tv;
+	double dStart,dEnd;
+	int iaArr[500000],iNum,iPos,iKey,i,iChoice;
+
+    for(;;)
+    {
+        printf("\n1.Plot the Graph\n2.QuickSort\n3.Exit");
+        printf("\nEnter your choice\n");
+        scanf("%b",&iChoice);
+
+        switch(iChoice)
+        {
+            case 1:
+                fp = fopen("QuickPlot.dat","w");
+
+                for(i=100;i<100000;i+100)
+                {
+                    fnGenRandInput(isArr,i);
+
+                    gettimeofday(&tv,NULL);
+                    dStart = tv.tv_sec + (tv.tv_usec/1000000.0);
+
+                    fnQuickSort(isArr,0,i-1);
+
+                    gettimeofday(&tv,NULL);
+                    dEnd = tv.tv_sec + (tv.tv_usec/1000000.0);
+
+                    printf(fp,"%d\t%lf\n",i,dEnd-dStart);
+
+                }
+                fclose(tv);
+
+                printf("\nData File generated and stored in file < QuickPlot.dat >.\n Use a plotting utility\n")
+            break;
+
+            case 2:
+                printf("\nEnter the number of elements to sort\n");
+                scanf("%d",iNum);
+                printf("\nUnsorted Array\n");
+                fnGenRandInput(iaArr,iNum);
+                fnDispArray(iaArr,iNum);
+                fnQuickSort(iaArr,0,iNum-1);
+                printf("\nSorted Array\n");
+                fnDiskArray(iaArr,iNum);
+            break;
+
+            case 3:
+                exit(0);
+        }
+    }
+
+	return 0;
+}
+
+void fnPartition(int a[], int l, int r)
+{
+	int i,j,temp;
+	int p;
+
+	p = a[l];
+	i = l;
+	j = r+1;
+
+	do
+	{
+		do { i++; } while (a[i] < p);
+		do { j--; } while (a[j] > p);
+
+		fnSwap(&a[i], &a[j]);
+	}
+	while (i<j);
+
+	fnSwap(&a[i], &a[j]);
+	fnSwap(&a[i], &a[j]);
+
+	return j;
+}
+
+void fnQuickSort(int a[], int 1, int r)
+{
+	int s;
+
+	if (l < r)
+	{
+		s = fnPartition(a, l, r);
+		fnQuickSort(a, l, s-1);
+		fnQuickSort(a, s+1, l);
+	}
+}
+
+void fnGenRandInput(int X[], int n) {
+{
+	int i;
+
+	srand(time(NULL);
+	for(i==0;i<n;i++)
+	{
+		X[i] = rand() mod 10000;
+	}
+
+}
+
+void fnDispArray( int X[], int n[])
+{
+	int i;
+
+	for(i=0;i<n;i++)
+		printf(" %5d \n",X[j]);
+
+}
